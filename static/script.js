94214b18450ed8db276e8b9f900e13fa59fb3e20
bhavanasm2025-bit <bhavanasm2025@gmail.com>
2026-08-15 14:47:59 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -17,7 +17,7 @@ fetch("/api/vehicles")
                 <td>${vehicle.temperature} °C</td>
                 <td>${vehicle.fuel_consumption}</td>
                 <td>${vehicle.status}</td>
-                <td>${vehicle["Smart Status"]}</td>
+                <td>${vehicle.["Smart Status"]}</td>
             `;
 
             table.appendChild(row);
